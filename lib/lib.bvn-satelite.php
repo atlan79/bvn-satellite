be948f214cbbe605475e5900a4c51fce61d3cb2e
atlan79 <th.winter@gmail.com>
2014-02-07 16:30:35 +0100

    Update lib.bvn-satelite.php
    WordPress Plugin for interacting with the BVN.ch API

--- lib/lib.bvn-satelite.php
+++ lib/lib.bvn-satelite.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * BVN Satelite
+ * BVN Satellite
  * 
  * WordPress Plugin for interacting with the BVN.ch API
  * 
  * @author Thomas Winter
- * @license https://github.com/atlan79/bvn-satelite/blob/master/LICENSE.md MIT
- * @version 0.6 beta
+ * @license https://github.com/atlan79/bvn-satellite/blob/master/LICENSE.md MIT
+ * @version 1.2
  */ 
  
 /**
@@ -18,23 +18,32 @@ function bvnsat_parseRankingWidget ($objRanking, $highlight) {
 		echo '<table class="noborder" style="width:260px">';
 		echo '<thead>';
 		echo '<tr>';
-		echo '<td class="nopadding">Mannschaft</td>';
-		echo '<td class="nopadding">S</td>';
-		echo '<td class="nopadding">N</td>';
+		echo '<td class="nopadding">Team</td>';
+		echo '<td class="nopadding">Sp</td>';
+		echo '<td class="nopadding">Si</td>';
+		echo '<td class="nopadding">Ni</td>';
+		echo '<td class="nopadding">Pu</td>';
+		echo '<td class="nopadding">Di</td>';
 		echo '</tr>';
 		echo '</thead>';
 		foreach ($objRanking as $team) {
 			if($highlight==$team->teamName) {
 				echo '<tr>';
-				echo '<td class="nopadding" style="font-weight: bold;">'.$team->teamName.'</td>';
+				echo '<td class="nopadding" style="font-weight: bold;" title="'.$team->teamName.'">'.$team->team5lc.'</td>';
+				echo '<td class="nopadding" style="font-weight: bold;">'.$team->games.'</td>';
 				echo '<td class="nopadding" style="font-weight: bold;">'.$team->win.'</td>';
 				echo '<td class="nopadding" style="font-weight: bold;">'.$team->lost.'</td>';
+				echo '<td class="nopadding" style="font-weight: bold;">'.$team->points.'</td>';
+				echo '<td class="nopadding" style="font-weight: bold;">'.$team->difference.'</td>';
 				echo '</tr>';
 			} else {
 				echo '<tr>';
-				echo '<td class="nopadding">'.$team->teamName.'</td>';
+				echo '<td class="nopadding" title="'.$team->teamName.'">'.$team->team5lc.'</td>';
+				echo '<td class="nopadding">'.$team->games.'</td>';
 				echo '<td class="nopadding">'.$team->win.'</td>';
 				echo '<td class="nopadding">'.$team->lost.'</td>';
+				echo '<td class="nopadding">'.$team->points.'</td>';
+				echo '<td class="nopadding">'.$team->difference.'</td>';
 				echo '</tr>';
 			}
 		}
@@ -47,38 +56,35 @@ function bvnsat_parseRankingWidget ($objRanking, $highlight) {
  * Parse Games
  * @param array with Schedule Data $arGames
   */
-function bvnsat_parseGamesWidget ($objGames, $api_realm, $api_id, $highlight) {
+function bvnsat_parseGamesWidget ($objGames, $api_realm, $api_id, $api_view, $highlight) {
 		echo '<div style="float:left">';
 		echo '<table class="noborder" style="width:260px">';
 		echo '<thead>';
 		echo '<tr>';
 		echo '<td class="nopadding">Datum</td>';
-		echo '<td class="nopadding">Mannschaft</td>';
+		echo '<td colspan="2" class="nopadding">Begegnung</td>';
 		echo '<td class="nopadding">Score</td>';
 		echo '</tr>';
 		echo '</thead>';
 		foreach ($objGames as $game) {
-			if($api_id==$game->homeTeamUrl) {
-				echo '<tr>';
-				echo '<td class="nopadding">'.$game->gameDay .'.'. $game->gameMonth.'.</td>';
-				echo '<td class="nopadding">'.$game->awayName.'</td>';
-				if($game->homescore != 0 &&  $game->awayscore != 0 ) {
-					echo '<td class="nopadding">'.$game->homescore .':'. $game->awayscore.'</td>';
-				} else {
-					echo '<td class="nopadding">&nbsp;</td>';
-				}
-				echo '</tr>';
+			echo '<tr>';
+			echo '<td class="nopadding">'.$game->gameDay .'.'. $game->gameMonth.'.</td>';
+			if($highlight==$game->homeName) {
+				echo '<td class="nopadding" style="font-weight: bold;" title="'.$game->homeName.'">'.$game->homeTeam5lc.'</td>';
 			} else {
-				echo '<tr>';
-				echo '<td class="nopadding">'.$game->gameDay .'.'. $game->gameMonth.'.</td>';
-				echo '<td class="nopadding">@'.$game->homeName.'</td>';
-				if($game->homescore != 0 &&  $game->awayscore != 0 ) {
-					echo '<td class="nopadding">'.$game->homescore .':'. $game->awayscore.'</td>';
-				} else {
-					echo '<td class="nopadding">&nbsp;</td>';
-				}
-				echo '</tr>';
+				echo '<td class="nopadding" title="'.$game->homeName.'">'.$game->homeTeam5lc.'</td>';
 			}
+			if($highlight==$game->awayName) {
+				echo '<td class="nopadding" style="font-weight: bold;" title="'.$game->awayName.'">'.$game->awayTeam5lc.'</td>';
+			} else {
+				echo '<td class="nopadding" title="'.$game->awayName.'">'.$game->awayTeam5lc.'</td>';
+			}
+			if($game->homescore != 0 &&  $game->awayscore != 0 ) {
+				echo '<td class="nopadding">'.$game->homescore .':'. $game->awayscore.'</td>';
+			} else {
+				echo '<td class="nopadding">&nbsp;</td>';
+			}
+			echo '</tr>';
 		}
 		echo '</table>';
 		echo '</div>';}
